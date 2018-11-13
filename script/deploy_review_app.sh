1167475833409b21b3c778cd07dbd73080287ee5
lucdejems <luc.dejems@gmail.com>
2018-11-13 15:24:29 +0100

    challenge modified

--- script/deploy_review_app.sh
+++ script/deploy_review_app.sh
@@ -1,3 +1,3 @@
-cho "{ \"alias\": \"$TRAVIS_BRANCH\"-${TRAVIS_REPO_SLUG/\//-} }" > now-review-app.json
+cho "{ \"alias\": \"$TRAVIS_BRANCH\"-echo ${TRAVIS_REPO_SLUG/\//-} }" > now-review-app.json
 now --public --token $NOW_TOKEN
 now alias --token $NOW_TOKEN --local-config now-review-app.json
